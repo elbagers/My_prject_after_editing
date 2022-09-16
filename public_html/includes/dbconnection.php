575a16ab15be27efa8ee499910fc151c8f680847
elbagers <elbagers@gmail.com>
2022-09-16 14:20:50 -0400

    first commit

---
+++ public_html/includes/dbconnection.php
@@ -0,0 +1,6 @@
+<?php
+$con=mysqli_connect("localhost", "id19544321_root", "?{11JnX%MqG3&8NY", "id19544321_bpms");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+?>
